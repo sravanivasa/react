c29d4f1ef4b07f55ee809cdb2b63aa4c1d45d5fa
sravani <vasasravani1103@gmail.com>
2024-09-19 15:13:12 +0530

    did some changes

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import React from 'react';
 
 const heading = React.createElement("h1",{id : "heading"},"hello world form React");
 // we need to put this heading in our div i.e root for this first create the root ele
 const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
